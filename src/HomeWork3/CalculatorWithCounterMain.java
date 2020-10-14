7608ab70a050e858aa51723090e7e4377ce4e919
Skazin <van00746@gmail.com>
2020-10-14 19:59:31 +0300

    Работа над ошибками 2.0

--- src/HomeWork3/CalculatorWithCounterMain.java
+++ src/HomeWork3/CalculatorWithCounterMain.java
@@ -8,15 +8,10 @@ public class CalculatorWithCounterMain {
     CalculatorWithCounter count = new CalculatorWithCounter(calc);
 
         double a = count.multiplication(15.0,7.0);
-        count.getCountOperation();
         double b = count.division(28.0, 5.0);
-        count.getCountOperation();
         double c = count.plus(a, b);
-        count.getCountOperation();
         double d = count.plus(4.1, c);
-        count.getCountOperation();
         double e = count.degree(d, 2);
-        long q = count.getCountOperation();
 
         System.out.println("15 * 7 = " + a);
         System.out.println("28 / 5 = " + b);
@@ -24,7 +19,7 @@ public class CalculatorWithCounterMain {
         System.out.println("4.1 + 15 * 7 + (28 / 5) = " + d);
         System.out.println("4.1 + 15 * 7 + (28 / 5) ^ 2 = " + e);
 
-        System.out.println("Количество использований калькулятора: " + q);
+        System.out.println("Количество использований калькулятора: " + count.getCountOperation());
 
     }
 }
