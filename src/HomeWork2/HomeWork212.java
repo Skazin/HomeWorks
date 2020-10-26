87c674f6fa96ea2e98d1bc77e4c4109544c4c204
Skazin <van00746@gmail.com>
2020-10-26 21:38:45 +0300

    исправлен 212 на работу с числом из аргумента к программе

--- src/HomeWork2/HomeWork212.java
+++ src/HomeWork2/HomeWork212.java
@@ -1,31 +1,32 @@
 package HomeWork2;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWork212 {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int a = in.nextInt();
-        in.close();
-        multiplication(a);
+        multiplication(args);
     }
-    static void multiplication (int a){
-        StringBuilder count = new StringBuilder(Integer.toString(a));
+    static void multiplication (String[] args){
+        StringBuilder count = new StringBuilder(Arrays.toString(args));
 
         for (int i = 0; i < count.length(); i++) {
             char b = count.charAt(i);
-            if (i == count.length() - 1) System.out.print(b + " ");
-            else System.out.print(b + " * ");
+            if (b != '[' && b!= ']'){
+                if (i == count.length() - 2) System.out.print(b + " ");
+                else System.out.print(b + " * ");
+            }
         }
         int d = 1;
+        int c = 0;
 
         for (int i = 0; i < count.length(); i++) {
             char b = count.charAt(i);
-            int c = (int) b - '0';
-            d *= c;
+            if (b != '[' && b!= ']'){
+                c = (int) b - '0';
+                d *= c;}
         }
         System.out.print("= " + d);
     }
